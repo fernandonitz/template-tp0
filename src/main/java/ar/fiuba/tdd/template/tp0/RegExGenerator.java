6a30fdbc40483c03477e4645719db67f79534f5d
fernandonitz <nitzfernando@gmail.com>
2016-03-21 10:44:17 -0300

    primer commit del tp0. Se deja el tp pasando todos los test, habiendo agregado otros unitarios y armando los ej de reg ex de forma aleatoria. Se pide perdón por haber commiteado todo de una, todavia me estoy acostumbrando a git...

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -1,24 +1,157 @@
 package ar.fiuba.tdd.template.tp0;
+import java.util.Random;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RegExGenerator {
-    // TODO: Uncomment this field
-    //private int maxLength;
+    static final int LONG_MAX = 65;
+    static final int CANT_CHAR = 256;
 
-    //public RegExGenerator(int maxLength) {
-    //    this.maxLength = maxLength;
-    //}
+    private int counter;
+    private String regEx;
 
     // TODO: Uncomment parameters
-    public List<String> generate(/*String regEx, int numberOfResults*/) {
-        return new ArrayList<String>() {
-            {
-                add("a");
-                add("b");
-                add("c");
-            }
-        };
+    public List<String> generate(String regEx, int numberOfResults) {
+        this.regEx = regEx;
+        ArrayList<String> examples = new ArrayList<String>();
+        //el warning acá es verdad que se puede cambiar, pero debido a que es así como se pide
+        // la salida del problema, no hay forma de solucionarlo...(warning inherente al problema)
+        for (int i=0;i<numberOfResults;i++)
+            examples.add(_generate());
+        return examples;
     }
+
+    public String _generate() {
+        StringBuilder example = new StringBuilder();
+        counter = 0;
+        while (counter<regEx.length()) {
+            if (regEx.charAt(counter) == '[')
+                example.append(caseCorchete());
+            else {
+                if (regEx.charAt(counter) == '.')
+                    example.append(caseDot());
+                else {
+                    if (regEx.charAt(counter) == '\\')
+                        example.append(caseBarra());
+                    else {
+                        example.append(caseChar());
+                    }
+                }
+            }
+        }
+        return example.toString();
+   }
+
+    boolean isCuantifier(char element){
+        return (element == '*' || element == '?' || element == '+');
+    }
+
+    String caseBarra(){
+        String result=null;
+        if((counter+1 != regEx.length()) && (counter+2 != regEx.length()) && isCuantifier(regEx.charAt(counter+2))){
+            result = getExCharCuantifier(regEx.charAt(counter+2),Character.toString(regEx.charAt(counter+1)));
+            counter+=3;
+        }
+        else{
+            if(counter+1 != regEx.length()){
+                result = Character.toString(regEx.charAt(counter+1));
+                counter+=2;
+            }
+        }
+
+        return result;
+    }
+
+    String caseChar(){
+        String result;
+        if((counter+1 != regEx.length()) && isCuantifier(regEx.charAt(counter+1))){
+            result = getExCharCuantifier(regEx.charAt(counter+1),Character.toString(regEx.charAt(counter)));
+            counter+=2;
+        }
+        else{
+            result = Character.toString(regEx.charAt(counter));
+            counter++;
+        }
+        return result;
+    }
+
+    String caseDot(){
+        String result;
+        if((counter+1 != regEx.length()) && isCuantifier(regEx.charAt(counter+1))){
+            result = getExDotCuantifier(regEx.charAt(counter+1));
+            counter+=2;
+        }
+        else{
+            result = getExDot();
+            counter++;
+        }
+        return result;
+    }
+
+    String caseCorchete(){
+        StringBuilder corcheteContent = new StringBuilder();
+        String result;
+        counter++;
+        while (regEx.charAt(counter) != ']'){
+            corcheteContent.append(regEx.charAt(counter));
+            counter++;
+        }
+        if((counter+1 != regEx.length()) && isCuantifier(regEx.charAt(counter+1))){
+            result = getExCorcheteCuantifier(regEx.charAt(counter+1),corcheteContent.toString());
+            counter+=2;
+        }
+        else{
+            result = getExCorchete(corcheteContent.toString());
+            counter++;
+        }
+        return result;
+    }
+
+    String getExDot(){
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(CANT_CHAR);
+        char randomIntToChar = (char) randomInt;
+        return Character.toString(randomIntToChar);
+    }
+
+    String getExCorchete(String elements){
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(elements.length());
+        return Character.toString(elements.charAt(randomInt));
+    }
+
+    String getExCorcheteCuantifier(char cuantifier, String elements){
+        StringBuilder finalString = new StringBuilder();
+        for (int i=0;i<getNumRepDependingOnCuantifier(cuantifier);i++){
+            finalString.append(getExCorchete(elements));
+        }
+        return finalString.toString();
+    }
+    String getExDotCuantifier(char cuantifier){
+        StringBuilder finalString = new StringBuilder();
+        for (int i=0;i<getNumRepDependingOnCuantifier(cuantifier);i++){
+            finalString.append(getExDot());
+        }
+        return finalString.toString();
+    }
+
+    String getExCharCuantifier(char cuantifier,String element){
+        StringBuilder finalString = new StringBuilder();
+        for (int i=0;i<getNumRepDependingOnCuantifier(cuantifier);i++){
+            finalString.append(element);
+        }
+        return finalString.toString();
+    }
+
+    int getNumRepDependingOnCuantifier(char cuantifier){
+        int numberOfRepetitionsOfCuantifier=0;
+        Random randomGenerator = new Random();
+        if(cuantifier == '*') numberOfRepetitionsOfCuantifier = randomGenerator.nextInt(LONG_MAX);
+        if(cuantifier == '?') numberOfRepetitionsOfCuantifier = randomGenerator.nextInt(2);
+        if(cuantifier == '+') numberOfRepetitionsOfCuantifier = randomGenerator.nextInt(LONG_MAX -1) + 1;
+        return numberOfRepetitionsOfCuantifier;
+    }
+
+
 }
--- src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -13,9 +13,10 @@ public class RegExGeneratorTest {
     private boolean validate(String regEx, int numberOfResults) {
         RegExGenerator generator = new RegExGenerator();
         // TODO: Uncomment parameters
-        List<String> results = generator.generate(/*regEx, numberOfResults*/);
+        List<String> results = generator.generate(regEx, numberOfResults);
         // force matching the beginning and the end of the strings
         Pattern pattern = Pattern.compile("^" + regEx + "$");
+
         return results
                 .stream()
                 .reduce(true,
@@ -24,10 +25,9 @@ public class RegExGeneratorTest {
                         return acc && matcher.find();
                     },
                     (item1, item2) -> item1 && item2);
+
     }
 
-    //TODO: Uncomment these tests
-    /*
     @Test
     public void testAnyCharacter() {
         assertTrue(validate(".", 1));
@@ -62,6 +62,55 @@ public class RegExGeneratorTest {
     public void testCharacterSetWithQuantifiers() {
         assertTrue(validate("[abc]+", 1));
     }
-    */
-    // TODO: Add more tests!!!
+
+    @Test
+    public void testIsCuantifierPlass() {
+        RegExGenerator generator = new RegExGenerator();
+        assertTrue(generator.isCuantifier('+'));
+    }
+    @Test
+    public void testIsCuantifierAsterisk() {
+        RegExGenerator generator = new RegExGenerator();
+        assertTrue(generator.isCuantifier('*'));
+    }
+    @Test
+    public void testIsCuantifierQuestion() {
+        RegExGenerator generator = new RegExGenerator();
+        assertTrue(generator.isCuantifier('?'));
+    }
+    @Test
+    public void testGetExDot() {
+        RegExGenerator generator = new RegExGenerator();
+        assertTrue((generator.getExDot()).length() == 1);
+    }
+    @Test
+    public void testGetExCorchete() {
+        RegExGenerator generator = new RegExGenerator();
+        String result = generator.getExCorchete("ab");
+        assertTrue(result.charAt(0) == 'a' || result.charAt(0) == 'b');
+    }
+    @Test
+    public void testCuantifierAnswer() {
+        RegExGenerator generator = new RegExGenerator();
+        String result = generator.getExCharCuantifier('?',"a");
+        assertTrue(result.length() == 0 || result.length() == 1);
+    }
+    @Test
+    public void testCuantifierPlass() {
+        RegExGenerator generator = new RegExGenerator();
+        String result = generator.getExCharCuantifier('+',"a");
+        assertTrue(result.length() >= 1 );
+    }
+    @Test
+    public void testCuantifierAsterisk() {
+        RegExGenerator generator = new RegExGenerator();
+        String result = generator.getExCharCuantifier('+',"a");
+        assertTrue(result.length() >= 0 );
+    }
+    @Test
+    public void testgenerate() {
+        RegExGenerator generator = new RegExGenerator();
+        List<String> results = generator.generate("a", 1);
+        assertTrue(results.get(0).charAt(0)=='a');
+    }
 }
